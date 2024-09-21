72cc6ac0986e992f99a47a63c7fe22cf3619caea
Nishantdas77 <dasnishant7777@gmail.com>
2024-09-21 22:33:11 +0530

    Home page is added

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -3,8 +3,12 @@ import style from './Home.module.css'
 
 const Page: React.FC = () => {
   return (
-    <div className={[style.you, style.l].join(" ")}>
-      Hello this is home page
+    <div className={[style.you].join(" ")}>
+      <div>
+      <h1 className={[style.l].join(" ")}>Code, Compete, Conquer!</h1>
+      <p className={[style.h].join(" ")}>Unleash your skills, climb the leaderboards, and emerge as the ultimate coding champion.</p>
+      </div>
+      
     </div>
   );
 }
