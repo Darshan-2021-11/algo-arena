14a0df9f1377c66af6980ffc68ce878ad28cab97
Nishantdas77 <dasnishant7777@gmail.com>
2024-09-22 01:47:01 +0530

    nav bar complete

--- src/app/utils/Nav.tsx
+++ src/app/utils/Nav.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import style from './Nav.module.css'
 
 const Nav: React.FC = () => {
   return (
-    <div>
-      Nav bar
+    <div className={[style.bg].join(" ")}>
+      <picture>
+        <img className={[style.img].join(" ")} src="/algoarena.png" alt="AlgoArena logo" />
+      </picture>
+      <div className={[style.p].join(" ")}><h6>Practice</h6></div>
+      <div className={[style.e].join(" ")}><h6>Explore</h6></div>
+      <div className={[style.l].join(" ")}><h6>Log in</h6></div>
     </div>
   )
 }
