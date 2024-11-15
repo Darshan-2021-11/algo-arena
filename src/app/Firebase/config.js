74bd6d1fd516b35c4bab14b5f6d20aa580eaf043
Nishantdas77 <dasnishant7777@gmail.com>
2024-11-15 20:18:29 +0530

    firestore added and user icon done

--- src/app/Firebase/config.js
+++ src/app/Firebase/config.js
@@ -1,18 +1,20 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
 console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: "algoarena-99000.firebaseapp.com",
-    projectId: "algoarena-99000",
-    storageBucket: "algoarena-99000.firebasestorage.app",
-    messagingSenderId: "27440046942",
-    appId: "1:27440046942:web:82cec2639d4b14c534a5cf",
-    measurementId: "G-G7LP8S7K8E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
   };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
 const auth = getAuth(app)
+const firestore = getFirestore(app)
 
-export {app, auth}
+export {app,firestore, auth}
