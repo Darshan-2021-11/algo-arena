a40bc08a9d1ef959d57b0ec6a2e2bec2981587c4
Nishantdas77 <dasnishant7777@gmail.com>
2025-02-10 23:06:47 +0530

    feat: Updated README.md

--- server/socket_methods/index.js
+++
@@ -1,9 +0,0 @@
-const { endMatch } = require("./endMatch");
-const { startMatch } = require("./startMatch");
-const submit = require("./submit");
-
-module.exports = {
-    startMatch,
-    endMatch,
-    submit
-}
